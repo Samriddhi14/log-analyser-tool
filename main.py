e99c8cddffffc7b5a9630064b4eaa0c6c5116fbd
Samriddhi14 <sam14.diva@gmail.com>
2024-06-18 20:35:36 +0530

    second commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 import re
 import pandas as pd
 import pprint
-logfile = open("logParser\serverlogs.log","r")
+logfile = open("serverlogs.log","r")
 
 
 pattern = r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
